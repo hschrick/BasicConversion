e5435acd4a6edc780d27efbdfda738d0b9376a45
Harris Schrick <hschrick@ksu.edu>
2017-02-09 13:51:35 -0800

    done

--- Basic Var Conversions/Program.cs
+++ Basic Var Conversions/Program.cs
@@ -10,6 +10,11 @@ namespace Basic_Var_Conversions
     {
         static void Main(string[] args)
         {
+            //c# can convert between instances of compatible types.
+
+            int x = 12345;  //int is a 32-bit integer
+            long y = x;     //implicit conversion to 64-bit integer
+            short z = (short)x; //Explicit conversion to 16-bit integer
         }
     }
 }
